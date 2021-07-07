d45394bd3a761d371c5d0cc5107b8a25e55cf741
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-07-07 10:44:09 -0700

    👩🏿‍💻 Update function

--- bootstrapping_tools/bootstrapping.py
+++ bootstrapping_tools/bootstrapping.py
@@ -10,7 +10,7 @@ def power_law(time_interval, population_growth_rate, initial_population_size):
 
 
 def lambda_calculator(
-    temporadas, maximo_nidos, max_iter=2000, lower_bounds=0, lambda_upper_bound=50
+    temporadas, maximo_nidos, max_iter=10000, lower_bounds=0, lambda_upper_bound=50
 ):
     temporadas = np.array(temporadas)
     numero_agno = temporadas - temporadas[0]
@@ -25,11 +25,22 @@ def lambda_calculator(
     return popt
 
 
-def remove_distribution_outliers(data):
+def remove_distribution_outliers(data, multiplier=2.5):
     data = np.array(data)
     mean = np.mean(data)
     std = np.std(data)
-    mask = abs(data - mean) < std * 5
+    mask = abs(data - mean) < std * multiplier
+    return data[mask]
+
+
+def tukey_fences(data, multiplier=1.5):
+    data = np.array(data)
+    first_quantile = np.quantile(data, 0.25)
+    third_quantile = np.quantile(data, 0.75)
+    interquartile_range = third_quantile - first_quantile
+    lower_limit = first_quantile - (interquartile_range * multiplier)
+    upper_limit = third_quantile + (interquartile_range * multiplier)
+    mask = (data > lower_limit) & (data < upper_limit)
     return data[mask]
 
 
@@ -47,7 +58,7 @@ def boostrapping_feature(data, N=2000):
     return bootstrap_data
 
 
-def lambdas_from_bootstrap_table(dataframe):
+def lambdas_from_bootstrap_table(dataframe, remove_outliers=True, outlier_method="tukey", **kwargs):
     lambdas_bootstraps = []
     seasons = np.array(dataframe.columns.values, dtype=int)
     N = len(dataframe)
@@ -55,10 +66,25 @@ def lambdas_from_bootstrap_table(dataframe):
     for i in tqdm(range(N)):
         fitting_result = lambda_calculator(seasons, dataframe.T[i].values)
         lambdas_bootstraps.append(fitting_result[0])
+    if remove_outliers == True:
+        if outlier_method == "tukey":
+            lambdas_bootstraps = tukey_fences(lambdas_bootstraps, **kwargs)
+        elif outlier_method == "std":
+            lambdas_bootstraps = remove_distribution_outliers(lambdas_bootstraps, **kwargs)
+        else:
+            raise Exception("No se reconoce el método de filtrado")
     return lambdas_bootstraps
 
 
-def lambdas_bootstrap_from_dataframe(dataframe, column_name, N=20, return_distribution=False):
+def lambdas_bootstrap_from_dataframe(
+    dataframe,
+    column_name,
+    N=2000,
+    return_distribution=False,
+    remove_outliers=True,
+    outlier_method="tukey",
+    **kwargs,
+):
     bootstraped_data = pd.DataFrame()
     lambdas_bootstraps = []
     seasons = dataframe.sort_values(by="Temporada").Temporada.unique()
@@ -67,7 +93,17 @@ def lambdas_bootstrap_from_dataframe(dataframe, column_name, N=20, return_distri
         data_per_season = dataframe[dataframe.Temporada == season]
         bootstraped_data[season] = boostrapping_feature(data_per_season[column_name], N)
     lambdas_bootstraps = lambdas_from_bootstrap_table(bootstraped_data)
-    return _return_distribution(return_distribution, lambdas_bootstraps)
+    if remove_outliers == True:
+        if outlier_method == "tukey":
+            lambdas_bootstraps = tukey_fences(lambdas_bootstraps, **kwargs)
+        elif outlier_method == "std":
+            lambdas_bootstraps = remove_distribution_outliers(lambdas_bootstraps, **kwargs)
+        else:
+            raise Exception("No se reconoce el método de filtrado")
+    if return_distribution == True:
+        return lambdas_bootstraps, np.percentile(lambdas_bootstraps, [2.5, 50, 97.5])
+    else:
+        return np.percentile(lambdas_bootstraps, [2.5, 50, 97.5])
 
 
 def get_bootstrap_interval(bootrap_interval):
@@ -77,23 +113,44 @@ def get_bootstrap_interval(bootrap_interval):
     return [inferior_limit, bootrap_interval[1], superior_limit]
 
 
-def bootstrap_from_time_series(dataframe, column_name, N=20, return_distribution=False):
+def bootstrap_from_time_series(
+    dataframe,
+    column_name,
+    N=2000,
+    return_distribution=False,
+    remove_outliers=True,
+    outlier_method="tukey",
+    **kwargs,
+):
     lambdas_bootstraps = []
+    cont = 0
+    rand = 0
     print("Calculating bootstrap growth rates distribution:")
-    for i in tqdm(range(N)):
+    while cont < N:
         resampled_data = dataframe.sample(
-            n=len(dataframe), replace=True, random_state=i
+            n=len(dataframe), replace=True, random_state=rand
         ).sort_index()
-        fitting_result = lambda_calculator(resampled_data["Temporada"], resampled_data[column_name])
+        try:
+            fitting_result = lambda_calculator(
+                resampled_data["Temporada"], resampled_data[column_name]
+            )
+        except RuntimeError:
+            rand += 1
+            continue
         lambdas_bootstraps.append(fitting_result[0])
-    lambdas_bootstraps = remove_distribution_outliers(lambdas_bootstraps)
-    return _return_distribution(return_distribution, lambdas_bootstraps)
-
-
-def _return_distribution(return_distribution, lambdas_bootstraps):
-    if return_distribution:
+        cont += 1
+        rand += 1
+    if remove_outliers == True:
+        if outlier_method == "tukey":
+            lambdas_bootstraps = tukey_fences(lambdas_bootstraps, **kwargs)
+        elif outlier_method == "std":
+            lambdas_bootstraps = remove_distribution_outliers(lambdas_bootstraps, **kwargs)
+        else:
+            raise Exception("No se reconoce el método de filtrado")
+    if return_distribution == True:
         return lambdas_bootstraps, np.percentile(lambdas_bootstraps, [2.5, 50, 97.5])
-    return np.percentile(lambdas_bootstraps, [2.5, 50, 97.5])
+    else:
+        return np.percentile(lambdas_bootstraps, [2.5, 50, 97.5])
 
 
 def calculate_p_values(distribution):
@@ -101,3 +158,8 @@ def calculate_p_values(distribution):
     mask = distribution < 1
     mask2 = distribution > 1
     return mask.sum() / len(distribution), mask2.sum() / len(distribution)
+
+
+def generate_latex_interval_string(intervals):
+    lower_limit, central, upper_limit = get_bootstrap_interval(intervals)
+    return f"${{{central}}}_{{-{lower_limit}}}^{{+{upper_limit}}}$"

71a8989475a3effed7a4fce1109332df472abf46
Mario Villasante <mario.villasante@islas.org.mx>
2022-06-02 15:20:08 -0700

    ♻️  Refactor variables and functions in class

--- bootstrapping_tools/resample_by_blocks.py
+++ bootstrapping_tools/resample_by_blocks.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 
-def resample_data_by_blocks(original_sample, block_numbers, blocks_length):
+def resample_data_by_blocks(original_sample, block_labels, blocks_length):
     n_rows_original = len(original_sample)
-    rows = get_rows(block_numbers, n_rows_original, blocks_length)
+    rows = get_rows(block_labels, n_rows_original, blocks_length)
     resample = original_sample.loc[rows, :]
     return resample
 
 
-def get_rows(block_numbers, n_rows_data, blocks_length):
+def get_rows(block_labels, n_rows_data, blocks_length):
     aux = np.arange(n_rows_data)
     rows = []
-    for i in block_numbers:
+    for i in block_labels:
         rows.extend(aux[0 + i : blocks_length + i])
     return rows
--- tests/test_resample_by_blocks.py
+++ tests/test_resample_by_blocks.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pandas.testing import assert_frame_equal
 from bootstrapping_tools import (
     resample_data_by_blocks,
@@ -9,47 +10,47 @@ def test_resample_data_by_blocks():
     blocks_length = 2
     block_labels = [0, 0]
     block_size_2 = Tester_By_Size_Blocks(block_labels, blocks_length)
-    data = pd.DataFrame({"a": [10, 20, 30], "b": [40, 60, 80]})
-    expected = pd.DataFrame({"a": [10, 20, 10, 20], "b": [40, 60, 40, 60]})
-    block_size_2.assert_resampled_by_blocks(data, expected)
-    expected = pd.DataFrame({"a": [10, 20, 20, 30], "b": [40, 60, 60, 80]})
+    block_size_2.set_expected([10, 20, 10, 20], [600, 700, 600, 700])
+    block_size_2.assert_resampled_by_blocks()
     block_size_2.block_labels = [0, 1]
-    block_size_2.assert_resampled_by_blocks(data, expected)
-    expected = pd.DataFrame({"a": [20, 30, 10, 20], "b": [60, 80, 40, 60]})
+    block_size_2.set_expected([10, 20, 20, 30], [600, 700, 700, 800])
+    block_size_2.assert_resampled_by_blocks()
     block_size_2.block_labels = [1, 0]
-    block_size_2.assert_resampled_by_blocks(data, expected)
+    block_size_2.set_expected([20, 30, 10, 20], [700, 800, 600, 700])
+    block_size_2.assert_resampled_by_blocks()
     block_size_2.block_labels = [0, 0, 1]
-    data = pd.DataFrame({"a": [1, 2, 3, 4, 5], "b": [4, 6, 8, 10, 12]})
-    expected = pd.DataFrame({"a": [1, 2, 1, 2, 2, 3], "b": [4, 6, 4, 6, 6, 8]})
-    block_size_2.assert_resampled_by_blocks(data, expected)
+    block_size_2.set_expected([10, 20, 10, 20, 20, 30], [600, 700, 600, 700, 700, 800])
+    block_size_2.assert_resampled_by_blocks()
     block_size_2.block_labels = [0, 2, 3]
-    expected = pd.DataFrame({"a": [1, 2, 3, 4, 4, 5], "b": [4, 6, 8, 10, 10, 12]})
-    block_size_2.assert_resampled_by_blocks(data, expected)
+    block_size_2.set_expected([10, 20, 30, 40, 40, 50], [600, 700, 800, 900, 900, 1000])
+    block_size_2.assert_resampled_by_blocks()
 
 
 def test_resample_data_by_blocks_of_size_3():
     blocks_length = 3
     block_labels = [0, 1]
     block_size_3 = Tester_By_Size_Blocks(block_labels, blocks_length)
-    data = _data_from_columns([1, 2, 3, 4, 5], [4, 6, 8, 10, 12])
-    expected = _data_from_columns([1, 2, 3, 2, 3, 4], [4, 6, 8, 6, 8, 10])
-    block_size_3.assert_resampled_by_blocks(data, expected)
-
-
-def assert_resampled_by_blocks(block_labels, data, expected, blocks_length):
-    obtained = resample_data_by_blocks(data, block_labels, blocks_length)
-    assert_frame_equal(expected.reset_index(drop=True), obtained.reset_index(drop=True))
+    block_size_3.set_expected([10, 20, 30, 20, 30, 40], [600, 700, 800, 700, 800, 900])
+    block_size_3.assert_resampled_by_blocks()
 
 
 class Tester_By_Size_Blocks:
     def __init__(self, block_labels, blocks_length):
         self.block_labels = block_labels
         self.blocks_length = blocks_length
+        self.expected = None
+        self.data = None
+        self._set_data()
 
-    def assert_resampled_by_blocks(self, data, expected):
-        obtained = resample_data_by_blocks(data, self.block_labels, self.blocks_length)
-        assert_frame_equal(expected.reset_index(drop=True), obtained.reset_index(drop=True))
+    def assert_resampled_by_blocks(self):
+        obtained = resample_data_by_blocks(self.data, self.block_labels, self.blocks_length)
+        assert_frame_equal(self.expected.reset_index(drop=True), obtained.reset_index(drop=True))
 
+    def set_expected(self, column_a, column_b):
+        self.expected = self._data_from_columns(column_a, column_b)
 
-def _data_from_columns(column_a, column_b):
-    return pd.DataFrame({"a": column_a, "b": column_b})
+    def _set_data(self):
+        self.data = self._data_from_columns(np.arange(10, 60, 10), np.arange(600, 1100, 100))
+
+    def _data_from_columns(self, column_a, column_b):
+        return pd.DataFrame({"a": column_a, "b": column_b})

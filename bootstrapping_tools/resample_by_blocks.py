bddd257dc691bc3cddcbd1f603383c90b1a7d0c6
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-10 14:29:00 -0700

    🛑 Add '_get_labels()'

--- bootstrapping_tools/resample_by_blocks.py
+++ bootstrapping_tools/resample_by_blocks.py
@@ -1,17 +1,21 @@
 import numpy as np
 
 
-def resample_data_by_blocks(original_sample, block_labels, blocks_length):
+def resample_data_by_blocks(original_sample, blocks_length):
     n_rows_original = len(original_sample)
+    block_labels = _get_labels(n_rows_original, blocks_length)
     rows = _get_rows(block_labels, n_rows_original, blocks_length)
     resample = original_sample.loc[rows, :]
     return resample
 
 
+def _get_labels(n_rows_original, blocks_length):
+    blocks_number = np.ceil(n_rows_original / blocks_length)
+    block_labels = np.arange(blocks_number)
+    return block_labels
+
+
 def _get_rows(block_labels, n_rows_data, blocks_length):
-    blocks_number = np.ceil(n_rows_data / blocks_length)
-    lenght_block_labels = len(block_labels)
-    assert lenght_block_labels <= blocks_number
     aux = np.arange(n_rows_data)
     rows = []
     for i in block_labels:

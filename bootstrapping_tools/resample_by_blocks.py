2a693bdc1012b2a3fa210b6d1750cf73e1494306
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-06-10 11:52:03 -0700

    🛑🧪 Add failing test

--- bootstrapping_tools/resample_by_blocks.py
+++ bootstrapping_tools/resample_by_blocks.py
@@ -9,6 +9,9 @@ def resample_data_by_blocks(original_sample, block_labels, blocks_length):
 
 
 def _get_rows(block_labels, n_rows_data, blocks_length):
+    blocks_number = np.ceil(n_rows_data/blocks_length)
+    lenght_block_labels = len(block_labels)
+    assert lenght_block_labels <= blocks_number
     aux = np.arange(n_rows_data)
     rows = []
     for i in block_labels:

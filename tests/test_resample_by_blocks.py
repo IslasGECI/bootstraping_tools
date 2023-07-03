95dbdf79bfa1fe9ff0ffc4b9ced35d74d3cb438b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-03 18:54:11 +0000

    🛑🧪 Fail tests

--- tests/test_resample_by_blocks.py
+++ tests/test_resample_by_blocks.py
@@ -8,7 +8,7 @@ import random
 def test_resample_and_shift_data():
     blocks_length = 2
     block_size_2 = Size_blocks_tester(blocks_length)
-    block_size_2.set_expected([20, 30, 20, 30, 0, 10], [800, 900, 800, 900, 600, 700])
+    block_size_2.set_expected([800, 900, 800, 900, 600, 700], [20, 30, 20, 30, 0, 10])
     random_seed = 2
     block_size_2.assert_resample_and_shift_data(random_seed)
 
@@ -17,7 +17,7 @@ def test_resample_and_shift_data_block_length_4():
     blocks_length = 4
     block_size_4 = Size_blocks_tester(blocks_length)
     block_size_4.set_expected(
-        [10, 20, 30, 40, 10, 20, 30, 40], [700, 800, 900, 1000, 700, 800, 900, 1000]
+        [700, 800, 900, 1000, 700, 800, 900, 1000], [10, 20, 30, 40, 10, 20, 30, 40]
     )
     random_seed = 2
     block_size_4.assert_resample_and_shift_data(random_seed)
@@ -26,7 +26,7 @@ def test_resample_and_shift_data_block_length_4():
 def test_random_resample_data_by_blocks_blocks_length_2():
     blocks_length = 2
     block_size_2 = Size_blocks_tester(blocks_length)
-    block_size_2.set_expected([30, 40, 30, 40, 10, 20], [800, 900, 800, 900, 600, 700])
+    block_size_2.set_expected([800, 900, 800, 900, 600, 700], [30, 40, 30, 40, 10, 20])
     random_seed = 2
     rng = random.Random(random_seed)
     block_size_2.assert_random_resampled_by_blocks(rng)
@@ -36,7 +36,7 @@ def test_random_resample_data_by_blocks():
     blocks_length = 4
     block_size_4 = Size_blocks_tester(blocks_length)
     block_size_4.set_expected(
-        [20, 30, 40, 50, 20, 30, 40, 50], [700, 800, 900, 1000, 700, 800, 900, 1000]
+        [700, 800, 900, 1000, 700, 800, 900, 1000], [20, 30, 40, 50, 20, 30, 40, 50]
     )
     random_seed = 2
     rng = random.Random(random_seed)
@@ -47,7 +47,7 @@ def test_length_block_labels():
     blocks_length = 5
     lenght_block_labels = 1
     block_size_5 = Size_blocks_tester(blocks_length)
-    block_size_5.set_expected([10, 20, 30, 40, 50], [600, 700, 800, 900, 1000])
+    block_size_5.set_expected([600, 700, 800, 900, 1000], [10, 20, 30, 40, 50])
     n_rows_data = len(block_size_5.data)
     blocks_number = np.ceil(n_rows_data / blocks_length)
     assert lenght_block_labels <= blocks_number
@@ -76,4 +76,4 @@ class Size_blocks_tester:
         self.data = self._data_from_columns(np.arange(10, 60, 10), np.arange(600, 1100, 100))
 
     def _data_from_columns(self, column_a, column_b):
-        return pd.DataFrame({"a": column_a, "b": column_b})
+        return pd.DataFrame({"b": column_b, "a": column_a})

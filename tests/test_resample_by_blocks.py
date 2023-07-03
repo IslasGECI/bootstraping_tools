04f0fde3a44093aac15e625d1d04d4e369c1ad60
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-03 16:53:40 +0000

    ♻️  Refactor

--- tests/test_resample_by_blocks.py
+++ tests/test_resample_by_blocks.py
@@ -7,7 +7,7 @@ import random
 
 def test_resample_and_shift_data():
     blocks_length = 2
-    block_size_2 = Tester_By_Size_Blocks(blocks_length)
+    block_size_2 = Size_blocks_tester(blocks_length)
     block_size_2.set_expected([20, 30, 20, 30, 0, 10], [800, 900, 800, 900, 600, 700])
     random_seed = 2
     block_size_2.assert_resample_and_shift_data(random_seed)
@@ -15,7 +15,7 @@ def test_resample_and_shift_data():
 
 def test_resample_and_shift_data_block_length_4():
     blocks_length = 4
-    block_size_4 = Tester_By_Size_Blocks(blocks_length)
+    block_size_4 = Size_blocks_tester(blocks_length)
     block_size_4.set_expected(
         [10, 20, 30, 40, 10, 20, 30, 40], [700, 800, 900, 1000, 700, 800, 900, 1000]
     )
@@ -25,7 +25,7 @@ def test_resample_and_shift_data_block_length_4():
 
 def test_random_resample_data_by_blocks_blocks_length_2():
     blocks_length = 2
-    block_size_2 = Tester_By_Size_Blocks(blocks_length)
+    block_size_2 = Size_blocks_tester(blocks_length)
     block_size_2.set_expected([30, 40, 30, 40, 10, 20], [800, 900, 800, 900, 600, 700])
     random_seed = 2
     rng = random.Random(random_seed)
@@ -34,7 +34,7 @@ def test_random_resample_data_by_blocks_blocks_length_2():
 
 def test_random_resample_data_by_blocks():
     blocks_length = 4
-    block_size_4 = Tester_By_Size_Blocks(blocks_length)
+    block_size_4 = Size_blocks_tester(blocks_length)
     block_size_4.set_expected(
         [20, 30, 40, 50, 20, 30, 40, 50], [700, 800, 900, 1000, 700, 800, 900, 1000]
     )
@@ -46,14 +46,14 @@ def test_random_resample_data_by_blocks():
 def test_length_block_labels():
     blocks_length = 5
     lenght_block_labels = 1
-    block_size_5 = Tester_By_Size_Blocks(blocks_length)
+    block_size_5 = Size_blocks_tester(blocks_length)
     block_size_5.set_expected([10, 20, 30, 40, 50], [600, 700, 800, 900, 1000])
     n_rows_data = len(block_size_5.data)
     blocks_number = np.ceil(n_rows_data / blocks_length)
     assert lenght_block_labels <= blocks_number
 
 
-class Tester_By_Size_Blocks:
+class Size_blocks_tester:
     def __init__(self, blocks_length):
         self.blocks_length = blocks_length
         self.expected = None

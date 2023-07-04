207bff1b07205ee91c148d4796eb0fe41267f51d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-04 16:51:58 +0000

    🛑🧪 Fail tests

--- tests/test_n0_and_lambdas_intervals.py
+++ tests/test_n0_and_lambdas_intervals.py
@@ -4,4 +4,6 @@ from bootstrapping_tools import get_percentile
 def tests_get_percentile():
     lambdas_n0s = [(3.1, 5), (1.2, 3), (2, 4)]
     limits = [2.5, 50, 97.5]
-    get_percentile(lambdas_n0s, limits)
+    obtained = get_percentile(lambdas_n0s, limits)
+    expected = [(1.2, 3), (2, 4), (3.1, 5)]
+    assert obtained == expected

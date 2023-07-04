22ddd8d250ca70771d128eb564dd3d57af9685e2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-04 17:52:53 +0000

    ✅ Pass tests

--- bootstrapping_tools/n0_and_lambdas_intervals.py
+++ bootstrapping_tools/n0_and_lambdas_intervals.py
@@ -4,6 +4,6 @@ from math import floor
 def get_percentile(lambdas_n0s, limits):
     sorted_tuples = sorted(lambdas_n0s)
     divided_limits = [limit / 100 for limit in limits]
-    length_list = len(lambdas_n0s)
-    indexes = [floor(limit * length_list) for limit in divided_limits]
+    length_list = len(lambdas_n0s) - 1
+    indexes = [round(limit * length_list) for limit in divided_limits]
     return [sorted_tuples[i] for i in indexes]
--- tests/test_n0_and_lambdas_intervals.py
+++ tests/test_n0_and_lambdas_intervals.py
@@ -15,5 +15,5 @@ def tests_get_percentile():
 
     lambdas_n0s = [(0, 7), (0, 7), (0, 3), (3.1, 5), (4, 6), (1.2, 3), (2, 4), (0, 7)]
     obtained = get_percentile(lambdas_n0s, limits)
-    expected = [(0, 3), (0, 7), (4, 6)]
+    expected = [(0, 3), (1.2, 3), (4, 6)]
     assert obtained == expected

2d3bfaa121f233c216e3106497c33a05b3be0a06
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-07-05 23:47:37 +0000

    ♻️  Refactor

--- bootstrapping_tools/resample_by_blocks.py
+++ bootstrapping_tools/resample_by_blocks.py
@@ -11,6 +11,16 @@ def random_resample_data_by_blocks(original_sample, blocks_length, rng):
     return resample
 
 
+def xxrandom_resample_data_by_blocks(original_sample, blocks_length, rng):
+    n_rows_original = len(original_sample)
+    block_labels = xxget_labels(n_rows_original, blocks_length)
+    number_of_blocks_to_choose = int(np.ceil(n_rows_original / blocks_length))
+    choosen_block_labels = rng.choices(block_labels, k=number_of_blocks_to_choose)
+    rows = get_rows(choosen_block_labels, n_rows_original, blocks_length)
+    resample = original_sample.iloc[rows, :].reset_index(drop=True)
+    return resample
+
+
 def get_labels(n_rows_original, blocks_length):
     blocks_number = n_rows_original - blocks_length + 1
     block_labels = np.arange(blocks_number, dtype=int)

2de186ee2a68894976160e51f01fd0b6cbf48c0e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-21 17:19:43 +0000

    🛑🧪 Fail tests

---
+++ tests/test_calculate_eradication_progress.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+from bootstrapping_tools import Bootstrap_from_time_series_parametrizer, ProgressBootstrapper
+
+raw_data = pd.DataFrame(
+    {
+        "Esfuerzo": [1 / 19.5, 2 / 19, 3 / 18.5, 4 / 18, 5 / 17.5, 6 / 17],
+        "Capturas": [1, 2, 3, 4, 5, 6],
+    }
+)
+
+
+def test_ProgressBootstrapper():
+    parametrizer = Bootstrap_from_time_series_parametrizer(
+        blocks_length=1, column_name="CPUE", N=100
+    )
+    parametrizer.set_data(raw_data)
+    bootstrapper = ProgressBootstrapper(parametrizer)

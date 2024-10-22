3795a2d60da9dc71361b17a8e4a843d47b8bb918
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-22 21:24:05 +0000

    ♻️  Refactor

--- bootstrapping_tools/__init__.py
+++ bootstrapping_tools/__init__.py
@@ -4,5 +4,6 @@ __version__ = "3.1.0"
 from .bootstrapping import *  # noqa
 from .n0_and_lambdas_intervals import *  # noqa
 from .resample_by_blocks import *  # noqa
+from .abstract_series_bootstrapper import *  # noqa
 from .calculate_growth_rates import *  # noqa
 from .calculate_eradication_progress import *  # noqa
--- bootstrapping_tools/calculate_growth_rates.py
+++ bootstrapping_tools/calculate_growth_rates.py
@@ -1,29 +1,13 @@
 from bootstrapping_tools.bootstrapping import (
     bootstrap_from_time_series,
-    calculate_intervals_from_p_values_and_alpha,
-    calculate_p_values,
-    generate_latex_interval_string,
     get_bootstrap_deltas,
     lambda_calculator,
     power_law,
 )
 
+from bootstrapping_tools.abstract_series_bootstrapper import AbstractSeriesBootstrapper
+
 import json
-import numpy as np
-from abc import ABC, abstractmethod
-
-
-def calculate_seasons_intervals(seasons):
-    years = []
-    first_index = 0
-    for index in np.where(np.diff(seasons) != 1)[0]:
-        if seasons[first_index] == seasons[index]:
-            years.append(f"{seasons[index]}")
-        else:
-            years.append(f"{seasons[first_index]}-{seasons[index]}")
-        first_index = index + 1
-    years.append(f"{seasons[first_index]}-{seasons[-1]}")
-    return years
 
 
 class Bootstrap_from_time_series_parametrizer:
@@ -58,73 +42,6 @@ def fit_population_model(seasons_series, data_series):
         parameters[1],
     )
     return model
-
-
-class AbstractSeriesBootstrapper(ABC):
-
-    @abstractmethod
-    def get_parameters_distribution(self):
-        pass
-
-    @property
-    def interval_lambdas(self):
-        return [interval[0] for interval in self.intervals]
-
-    @property
-    def p_values(self):
-        lambdas = [lambdas_n0[0] for lambdas_n0 in self.parameters_distribution]
-        p_value_mayor, p_value_menor = calculate_p_values(lambdas)
-        p_values = (p_value_mayor, p_value_menor)
-        return p_values
-
-    @property
-    def intervals(self):
-        intervals = calculate_intervals_from_p_values_and_alpha(
-            self.parameters_distribution, self.p_values, self.bootstrap_config["alpha"]
-        )
-        return intervals
-
-    @property
-    def lambda_latex_interval(self):
-        lambda_latex_string = generate_latex_interval_string(
-            self.interval_lambdas, deltas=False, **{"decimals": 2}
-        )
-        return lambda_latex_string
-
-    def generate_season_interval(self):
-        return "({}-{})".format(
-            self.season_series.min(axis=0),
-            self.season_series.max(axis=0),
-        )
-
-    def get_monitored_seasons(self):
-        monitored_seasons = np.sort(self.season_series.astype(int).unique())
-        if len(monitored_seasons) == 1:
-            return f"{monitored_seasons[0]}"
-        else:
-            seasons_intervals = calculate_seasons_intervals(monitored_seasons)
-            return ",".join(seasons_intervals)
-
-    def get_parameters_dictionary(self):
-        json_dict = {
-            "intervals": list(self.intervals),
-            "main_parameter_latex_interval": self.lambda_latex_interval,
-            "p-values": self.p_values,
-            "bootstrap_intermediate_distribution": self.get_parameters_inside_confidence_interval(),
-        }
-        return json_dict
-
-    @abstractmethod
-    def save_intervals(self, output_path):
-        pass
-
-    def get_parameters_inside_confidence_interval(self):
-        return [
-            parameters_tuple
-            for parameters_tuple in self.parameters_distribution
-            if (parameters_tuple[0] > self.intervals[0][0])
-            and (parameters_tuple[0] < self.intervals[2][0])
-        ]
 
 
 class LambdasBootstrapper(AbstractSeriesBootstrapper):

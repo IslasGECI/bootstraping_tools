7d66fa3a64fced5bf65e7964a84635a32d7cb7bd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-22 21:18:17 +0000

    ♻️  Refactor

--- tests/test_calculate_growth_rates.py
+++ tests/test_calculate_growth_rates.py
@@ -24,7 +24,7 @@ def get_df(file_path):
 
 df = get_df("tests/data/dcco_laal_gumu_burrows_data.csv")
 laal = df[df["Nombre_en_ingles"] == "Laysan Albatross"]
-bootstrap_number = 100
+bootstrap_number = 10
 parametrizer = Bootstrap_from_time_series_parametrizer(blocks_length=2, N=bootstrap_number)
 parametrizer.set_data(laal)
 bootstraper = LambdasBootstrapper(parametrizer)
@@ -43,9 +43,9 @@ def test_save_intervals():
         "bootstrap_intermediate_distribution",
     ]
     assert set(obtained_fields) == set(expected_fields)
-    expected_intervals = [[1.11653, 150.30929], [1.21173, 77.48159], [1.4272, 4.56072]]
+    expected_intervals = [[1.1097, 128.85392], [1.2094, 68.65764], [1.4272, 4.56072]]
     assert_array_almost_equal(obtained_json["intervals"], expected_intervals, decimal=5)
-    expected_latex_interval = "1.21 (1.12 - 1.43)"
+    expected_latex_interval = "1.21 (1.11 - 1.43)"
     assert obtained_json["lambda_latex_interval"] == expected_latex_interval
     obtained_p_minor_value = obtained_json["p-values"][0]
     assert obtained_p_minor_value >= 0
@@ -70,11 +70,11 @@ def test_intervals_from_p_values_and_alpha():
     assert obtained_len_intervals == expected_len_intervals
     obtained_intervals_property = bootstraper.intervals
     expected_intervals = [
-        (1.03555254967221, 56.72689275689199),
-        (1.2199265239402008, 14.422599452094458),
-        (9.496750484649498, 2.419173122070242e-07),
+        [1.03555e00, 5.67269e01],
+        [1.18278e00, 2.89561e01],
+        [9.49983e00, 2.41214e-07],
     ]
-    assert_array_almost_equal(obtained_intervals_property, expected_intervals, decimal=5)
+    assert_array_almost_equal(obtained_intervals_property, expected_intervals, decimal=4)
 
 
 def test_generate_season_interval():

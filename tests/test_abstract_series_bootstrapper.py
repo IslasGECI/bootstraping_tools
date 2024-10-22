dce516366916a702bef6d7f7c8027f1745b84271
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-22 22:06:48 +0000

    🛑🧪 Fail tests

---
+++ tests/test_abstract_series_bootstrapper.py
@@ -0,0 +1,20 @@
+from bootstrapping_tools import AbstractSeriesBootstrapper, Bootstrap_from_time_series_parametrizer
+
+
+def test_abstract_bootstrapp():
+    AbstractSeriesBootstrapper.__abstractmethods__ = set()
+
+    class Dummy(AbstractSeriesBootstrapper):
+        def __init__(self, bootstrapper_parametrizer):
+            pass
+
+    independent_variable = "Capturas"
+    bootstrap_number = 100
+    parametrizer = Bootstrap_from_time_series_parametrizer(
+        blocks_length=1,
+        column_name="CPUE",
+        N=bootstrap_number,
+        independent_variable=independent_variable,
+    )
+    obtained = Dummy(parametrizer)
+    assert obtained.parameters_distribution == None
